be0fdbfe072cd11a5ab3805eec98b9396747d92c
Kuba Miszcz <KubaMiszcz@users.noreply.github.com>
2023-08-28 23:46:08 +0200

    layout improvements (#1)

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -4,32 +4,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
-export class MainComponent implements OnInit{
-  message='';
-  convertedMessage='';
-  deconvertedMessage='';
+export class MainComponent implements OnInit {
+  message = '';
+  convertedMessage = '';
+  deconvertedMessage = '';
 
-  constructor(private appService:AppService) {
-  }
+  constructor(private appService: AppService) {}
 
   ngOnInit(): void {
-      this.message="mission 226 tank A-398 start 34UEA12345 67892"
-      this.convertedMessage = this.appService.convertMessage(this.message);
+    this.message = 'mission 226 tank A-398 start 34UEA12345 67892';
+    this.convertedMessage = this.appService.convertMessage(this.message);
 
-     this.convertedMessage= this.convertedMessage.replace(/(.{5})/g,"$1 ")
+    this.convertedMessage = this.convertedMessage.replace(/(.{5})/g, '$1 ');
 
-           this.deconvertedMessage = this.appService.deconvertMessage(this.convertedMessage.replaceAll(' ',''));
-    }
-    
-    convertMessage(){
-      this.convertedMessage = this.appService.convertMessage(this.message);
-  }
-    deconvertMessage(){
-      this.deconvertedMessage = this.appService.deconvertMessage(this.convertedMessage);
+    this.deconvertedMessage = this.appService.deconvertMessage(
+      this.convertedMessage.replaceAll(' ', '')
+    );
   }
 
+  convertMessage() {
+    this.convertedMessage = this.appService.convertMessage(this.message);
+    this.convertedMessage = this.convertedMessage.replace(/(.{5})/g, '$1 ');
+  }
+  deconvertMessage() {
+    this.deconvertedMessage = this.appService.deconvertMessage(
+      this.convertedMessage.replaceAll(' ', '')
+    );
+  }
 
-
+  getConvertedMessageLength() {
+   return this.convertedMessage.replaceAll(' ', '').length;
+  }
 }
